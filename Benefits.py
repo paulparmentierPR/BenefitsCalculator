93da205a0b76b78f6d39859fdc9f274af473bb38
paulparmentierPR <145793022+paulparmentierPR@users.noreply.github.com>
2023-10-26 18:45:04 +0200

    Update Benefits.py

--- Benefits.py
+++ Benefits.py
@@ -17,6 +17,10 @@ percent_orders_easy24 = st.number_input("Enter % of Orders Going Through EASY24:
 orders_reduced = annual_orders * percent_orders_easy24
 direct_cost_savings = annual_cost_per_rep * sales_reps * percent_orders_easy24
 
+# Summary Box
+st.subheader("Summary")
+st.write(f"**Total Orders Reduced**: {orders_reduced:,}\n**Total Direct Cost Savings**: €{direct_cost_savings:,}")
+
 # Output Metrics
 st.subheader(f"Strategic Insights for {annual_orders:,} Orders & {sales_reps} Sales Reps")
 
@@ -25,7 +29,8 @@ fig1, ax1 = plt.subplots()
 labels1 = ['Total Orders', 'Orders Reduced']
 values1 = [annual_orders, -orders_reduced]
 cumulative_values1 = np.cumsum(values1)
-ax1.bar(labels1, cumulative_values1, color=['#1f77b4', '#d62728'])
+colors1 = ['#1f77b4', '#2ca02c']
+ax1.bar(labels1, cumulative_values1, color=colors1)
 ax1.set_title("Order Reduction Waterfall Chart")
 for i, value in enumerate(cumulative_values1):
     ax1.text(i, value, str(round(values1[i], 2)), ha='center', va='bottom')
@@ -36,12 +41,9 @@ fig2, ax2 = plt.subplots()
 labels2 = ['Baseline Cost (€)', 'Direct Cost Savings (€)']
 values2 = [annual_cost_per_rep * sales_reps, -direct_cost_savings]
 cumulative_values2 = np.cumsum(values2)
-ax2.bar(labels2, cumulative_values2, color=['#1f77b4', '#d62728'])
+colors2 = ['#1f77b4', '#2ca02c']
+ax2.bar(labels2, cumulative_values2, color=colors2)
 ax2.set_title("Monetary Savings Waterfall Chart")
 for i, value in enumerate(cumulative_values2):
     ax2.text(i, value, str(round(values2[i], 2)), ha='center', va='bottom')
 st.pyplot(fig2)
-
-# Summary Box
-st.subheader("Summary")
-st.write(f"**Total Orders Reduced**: {orders_reduced:,}\n**Total Direct Cost Savings**: €{direct_cost_savings:,}")
